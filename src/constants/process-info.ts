f77b49cd6ec495fe3ed3ef07711fe769c232ac1a
ian eddie <ianeddie2@gmail.com>
2024-05-28 21:13:47 -0300

    Form Section created

---
+++ src/constants/process-info.ts
@@ -0,0 +1,24 @@
+interface process {
+  number: string
+  title: string
+}
+type processData = process[]
+
+export const PROCESS_INFO: processData = [
+  {
+    number: '01',
+    title: '3D Conception & Design'
+  },
+  {
+    number: '02',
+    title: 'Interaction Design'
+  },
+  {
+    number: '03',
+    title: 'VR World User testing'
+  },
+  {
+    number: '04',
+    title: 'Hydra VR Deploy'
+  }
+]
