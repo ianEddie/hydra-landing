0a8200a5bfc5c7124565279386a382fd08529e95
ian eddie <ianeddie2@gmail.com>
2024-05-30 00:03:45 -0300

    Header created

---
+++ src/constants/header-navigation.ts
@@ -0,0 +1,18 @@
+export const HEADER_NAVIGATION = [
+  {
+    value: 'about',
+    href: '#about'
+  },
+  {
+    value: 'services',
+    href: '#services'
+  },
+  {
+    value: 'technologies',
+    href: '#technologies'
+  },
+  {
+    value: 'how to',
+    href: '#jj'
+  }
+]
