22e0d7d45ef20d139da65c08f6dd90ed863a107c
ian eddie <ianeddie2@gmail.com>
2024-05-29 22:28:21 -0300

    Footer created

---
+++ src/constants/footer-navigation.ts
@@ -0,0 +1,22 @@
+export const FOOTER_NAVIGATION = [
+  {
+    value: 'about',
+    href: '#about'
+  },
+  {
+    value: 'services',
+    href: '#services'
+  },
+  {
+    value: 'technologies',
+    href: '#technologies'
+  },
+  {
+    value: 'how to',
+    href: '#jj'
+  },
+  {
+    value: 'join hydra',
+    href: '#hh'
+  }
+]
